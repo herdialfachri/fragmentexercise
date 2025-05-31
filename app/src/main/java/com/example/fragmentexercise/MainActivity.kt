aebd37588ad4e6789915374dad2d6c9091d5561f
herdialfachri <alfachri75@gmail.com>
2025-05-31 23:11:55 +0700

    Adding bottom navigation menu

--- app/src/main/java/com/example/fragmentexercise/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexercise/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.fragmentexercise
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.fragmentexercise.botnav.DashboardFragment
+import com.example.fragmentexercise.botnav.HomeFragment
+import com.example.fragmentexercise.botnav.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Default Fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_dashboard -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, DashboardFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
